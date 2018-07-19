db2592f9492a558d694eaf0f103ef5f69f24204f
Joe Carmody <carmodyj1046@gmail.com>
2018-07-19 18:41:20 -0500

    Initial Commit

---
+++ StarCraft 2 Build Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarCraft 2 Build Timer
+//
+//  Created by Joe Carmody  on 7/19/18.
+//  Copyright © 2018 Joe Carmody . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
